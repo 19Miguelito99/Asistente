a1814e325e3657727c6a1f2bb853949e1841e82a
miguel <jperezm59@miumg.edu.gt>
2025-05-31 11:06:39 -0600

    actualización de código

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import modules.voice_input as vi
 import modules.voice_output as vo
 from modules.command_handler import procesar_comando
 
+PALABRA_CLAVE = "wanda"  # o "hey asistente"
+
 def main():
-    vo.speak("Asistente activado. ¿En qué puedo ayudarte?")
+    vo.speak("Asistente activado. Di 'wanda' para comenzar.")
     while True:
-        comando = vi.escuchar_comando()
-        if comando:
-            if "salir" in comando.lower():
-                vo.speak("Hasta luego.")
-                break
-            respuesta = procesar_comando(comando)
-            if respuesta:
-                vo.speak(respuesta)
+        print("Escuchando palabra clave...")
+        activacion = vi.escuchar_comando()
+        if activacion and PALABRA_CLAVE in activacion.lower():
+            vo.speak("Te escucho.")
+            comando = vi.escuchar_comando()
+
+            if comando:
+                if "salir" in comando.lower():
+                    vo.speak("Hasta luego.")
+                    break
+                respuesta = procesar_comando(comando)
+                if respuesta:
+                    vo.speak(respuesta)
 
 if __name__ == "__main__":
     main()
+
